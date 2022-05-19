8ed8e10bb4048d46a2f7e5aff30ead2e4cce7c14
VarML <104193216+VarML@users.noreply.github.com>
2022-05-20 03:42:16 +0900

    Add files via upload

---
+++ model/evaluation/evaluate_episodes.py
@@ -0,0 +1,313 @@
+import numpy as np
+import torch
+from itertools import count
+from sklearn.preprocessing import minmax_scale
+
+def evaluate_episode(
+        env,
+        state_dim,
+        act_dim,
+        model,
+        max_ep_len=1000,
+        device='cuda',
+        target_return=None,
+        state_mean=0.,
+        state_std=1.,
+):
+
+    model.eval()
+    model.to(device=device)
+
+    state_mean = torch.from_numpy(state_mean).to(device=device)
+    state_std = torch.from_numpy(state_std).to(device=device)
+
+    state = env.reset()
+    state = np.array(state)
+
+    # we keep all the histories on the device
+    # note that the latest action and reward will be "padding"
+    states = torch.from_numpy(state).reshape(1, state_dim).to(device=device, dtype=torch.float32)
+    actions = torch.zeros((0, act_dim), device=device, dtype=torch.float32)
+    rewards = torch.zeros(0, device=device, dtype=torch.float32)
+    sim_states = []
+
+    episode_return, episode_length = 0, 0
+    for t in count():
+
+        # add padding
+        actions = torch.cat([actions, torch.zeros((1, act_dim), device=device)], dim=0)
+        rewards = torch.cat([rewards, torch.zeros(1, device=device)])
+
+        action = model.get_action(
+            (states.to(dtype=torch.float32) - state_mean) / state_std,
+            actions.to(dtype=torch.float32),
+            rewards.to(dtype=torch.float32),
+            timesteps.to(dtype=torch.long)
+        )
+        actions[-1] = action
+        action = action.detach().cpu().numpy()
+
+        state, reward, done, _ = env.step(action)
+
+        cur_state = torch.from_numpy(state).to(device=device).reshape(1, state_dim)
+        states = torch.cat([states, cur_state], dim=0)
+        rewards[-1] = reward
+
+        episode_return += reward
+        episode_length += 1
+
+        if done:
+            break
+
+    return episode_return, episode_length
+
+
+def evaluate_episode_rtg(
+        env,
+        state_dim,
+        act_dim,
+        model,
+        max_ep_len=1000,
+        scale=1000.,
+        state_mean=0.,
+        state_std=1.,
+        device='cuda',
+        target_return=None
+    ):
+
+    model.eval()
+    model.to(device=device)
+    
+    state_mean = torch.from_numpy(state_mean).to(device=device)
+    state_std = torch.from_numpy(state_std).to(device=device)
+
+    state = env.reset()
+    state=np.array(state)
+
+    # we keep all the histories on the device
+    # note that the latest action and reward will be "padding"
+    states = torch.from_numpy(state).reshape(1, state_dim).to(device=device, dtype=torch.float32)
+    actions = torch.zeros((0, act_dim), device=device, dtype=torch.float32)
+    rewards = torch.zeros(0, device=device, dtype=torch.float32)
+
+    ep_return = target_return
+    target_return = torch.tensor(ep_return, device=device, dtype=torch.float32).reshape(1, 1)
+    timesteps = torch.tensor(0, device=device, dtype=torch.long).reshape(1, 1)
+
+    sim_states = []
+    #print(states)
+    episode_return, episode_length = 0, 0
+    for t in range(max_ep_len):
+
+        # add padding
+        actions = torch.cat([actions, torch.zeros((1, act_dim), device=device)], dim=0)
+        rewards = torch.cat([rewards, torch.zeros(1, device=device)])
+
+        action = model.get_action(
+            (states.to(dtype=torch.float32) - state_mean) / state_std,
+            actions.to(dtype=torch.float32),
+            rewards.to(dtype=torch.float32),
+            target_return.to(dtype=torch.float32),
+            timesteps.to(dtype=torch.long),
+        )
+        actions[-1] = action
+        action = action.detach().cpu().numpy()
+
+        state, reward, done, _ = env.step(action)
+        state=np.array(state)
+        cur_state = torch.from_numpy(state).to(device=device).reshape(1, state_dim)
+        states = torch.cat([states, cur_state], dim=0)
+        rewards[-1] = reward
+
+ 
+        pred_return = target_return[0,-1]
+        target_return = torch.cat(
+            [target_return, pred_return.reshape(1, 1)], dim=1)
+        timesteps = torch.cat(
+            [timesteps,
+             torch.ones((1, 1), device=device, dtype=torch.long) * (t+1)], dim=1)
+
+        episode_return += reward
+        episode_length += 1
+
+        if done:
+            break
+
+    return episode_return, episode_length
+
+
+def evaluate(
+        env,
+        state_dim,
+        act_dim,
+        model,
+        max_ep_len=1000,
+        scale=1000.,
+        device='cuda',
+        target_return=None,
+        mode='normal',
+):
+    model.eval()
+    model.to(device=device)
+
+    state = env.reset()
+
+    state = np.array(state)
+    if mode == 'noise':
+        state = state + np.random.normal(0, 0.1, size=state.shape)
+
+    # we keep all the histories on the device
+    # note that the latest action and reward will be "padding"
+    states = torch.from_numpy(state).reshape(1, state_dim).to(device=device, dtype=torch.float32)
+    actions = torch.zeros((0, act_dim), device=device, dtype=torch.float32)
+    rewards = torch.zeros(0, device=device, dtype=torch.float32)
+
+    ep_return = target_return
+    target_return = torch.tensor(ep_return, device=device, dtype=torch.float32).reshape(1, 1)
+    timesteps = torch.tensor(0, device=device, dtype=torch.long).reshape(1, 1)
+
+    sim_states = []
+
+    episode_return, episode_length = 0, 0
+    obs = []
+    next=[]
+    rews = []
+    news = []
+    acs = []
+    for t in count():
+        # add padding
+        actions = torch.cat([actions, torch.zeros((1, act_dim), device=device)], dim=0)
+        rewards = torch.cat([rewards, torch.zeros(1, device=device)])
+
+        ss = states.tolist()
+        l = []
+        for i in ss:
+            i = np.array(i)
+            i = minmax_scale(i)
+            l.append(i.tolist())
+        l = np.array(l)
+        ss = torch.from_numpy(l).reshape(-1, state_dim).to(device=device, dtype=torch.float32)
+
+        action = model.get_action(
+            ss.to(dtype=torch.float32),
+            actions.to(dtype=torch.float32),
+            rewards.to(dtype=torch.float32),
+            target_return.to(dtype=torch.float32),
+            timesteps.to(dtype=torch.long),
+        )
+        actions[-1] = action
+        action = action.detach().cpu().numpy()
+        action = (action + np.random.normal(0, 0.1, size=env.action_space.shape[0])).clip(
+            env.action_space.low, env.action_space.high)
+        next_state, reward, done, _ = env.step(action)
+        next_state = np.array(next_state)
+        cur_state = torch.from_numpy(next_state).to(device=device).reshape(1, state_dim)
+        states = torch.cat([states, cur_state], dim=0)
+        rewards[-1] = reward
+
+        if mode != 'delayed':
+            pred_return = target_return[0, -1] - (reward / scale)
+        else:
+            pred_return = target_return[0, -1]
+        target_return = torch.cat(
+            [target_return, pred_return.reshape(1, 1)], dim=1)
+        timesteps = torch.cat(
+            [timesteps,
+             torch.ones((1, 1), device=device, dtype=torch.long) * (t + 1)], dim=1)
+
+        episode_return += reward
+        episode_length += 1
+
+        rews.append(reward)
+        news.append(done)
+        obs.append(state)
+        next.append(next_state)
+        acs.append(action)
+        state=next_state
+
+        if done:
+            break
+    obs = np.array(obs)
+    next = np.array(next)
+    rews = np.array(rews)
+    news = np.array(news)
+    acs = np.array(acs)
+    
+    traj = {"observations": obs, "next_state":next,"rewards": rews, "dones": news, "actions": acs}
+    episode_data = {}
+    paths = []
+    for k in traj:
+        episode_data[k] = np.array(traj[k])
+    paths.append(episode_data)
+
+    return paths
+
+
+def eval_test(
+        env,
+        state_dim,
+        act_dim,
+        model,
+        max_ep_len=1000,
+        scale=1000.,
+        state_mean=0.,
+        state_std=1.,
+        device='cuda',
+        target_return=None,
+        mode='normal',
+    ):
+
+    model.eval()
+    model.to(device=device)
+
+    state_mean = torch.from_numpy(state_mean).to(device=device)
+    state_std = torch.from_numpy(state_std).to(device=device)
+
+    state = env.reset()
+    state = np.array(state)
+
+    if mode == 'noise':
+        state = state + np.random.normal(0, 0.1, size=state.shape)
+
+    # we keep all the histories on the device
+    # note that the latest action and reward will be "padding"
+    states = torch.from_numpy(state).reshape(1, state_dim).to(device=device, dtype=torch.float32)
+    actions = torch.zeros((0, act_dim), device=device, dtype=torch.float32)
+    rewards = torch.zeros(0, device=device, dtype=torch.float32)
+
+    timesteps = torch.tensor(0, device=device, dtype=torch.long).reshape(1, 1)
+
+    episode_return, episode_length = 0, 0
+    for t in range(max_ep_len):
+
+        # add padding
+        actions = torch.cat([actions, torch.zeros((1, act_dim), device=device)], dim=0)
+        rewards = torch.cat([rewards, torch.zeros(1, device=device)])
+
+        action = model.get_action(
+            (states.to(dtype=torch.float32) - state_mean) / state_std,
+            actions.to(dtype=torch.float32),
+            rewards.to(dtype=torch.float32),
+            timesteps.to(dtype=torch.long),
+        )
+        actions[-1] = action
+        action = action.detach().cpu().numpy()
+
+        state, reward, done, _ = env.step(action)
+        state = np.array(state)
+
+        cur_state = torch.from_numpy(state).to(device=device).reshape(1, state_dim)
+        states = torch.cat([states, cur_state], dim=0)
+        rewards[-1] = reward
+
+        timesteps = torch.cat(
+            [timesteps,
+             torch.ones((1, 1), device=device, dtype=torch.long) * (t+1)], dim=1)
+
+        episode_return += reward
+        episode_length += 1
+
+        if done:
+            break
+
+    return episode_return, episode_length
