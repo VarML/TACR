c27013aee669b30f6f7aae2dd29db165c19be7be
VarML <104193216+VarML@users.noreply.github.com>
2022-08-19 17:36:33 +0900

    Add files via upload

---
+++ preprocessor/preprocessors.py
@@ -0,0 +1,206 @@
+import datetime
+
+import numpy as np
+import pandas as pd
+from stock_env.apps import config
+from preprocessor.yahoodownloader import YahooDownloader
+from stockstats import StockDataFrame as Sdf
+
+
+def load_dataset(*, file_name: str) -> pd.DataFrame:
+    """
+    load csv dataset from path
+    :return: (df) pandas dataframe
+    """
+    # _data = pd.read_csv(f"{config.DATASET_DIR}/{file_name}")
+    _data = pd.read_csv(file_name)
+    return _data
+
+
+def data_split(df, start, end, target_date_col="date"):
+    """
+    split the dataset into training or testing using date
+    :param data: (df) pandas dataframe, start, end
+    :return: (df) pandas dataframe
+    """
+    data = df[(df[target_date_col] >= start) & (df[target_date_col] < end)]
+    data = data.sort_values([target_date_col, "tic"], ignore_index=True)
+    data.index = data[target_date_col].factorize()[0]
+    return data
+
+
+def convert_to_datetime(time):
+    time_fmt = "%Y-%m-%dT%H:%M:%S"
+    if isinstance(time, str):
+        return datetime.datetime.strptime(time, time_fmt)
+
+
+class FeatureEngineer:
+    """Provides methods for preprocessing the stock price data
+
+    Attributes
+    ----------
+        use_technical_indicator : boolean
+            we technical indicator or not
+        tech_indicator_list : list
+            a list of technical indicator names (modified from neofinrl_config.py)
+        use_turbulence : boolean
+            use turbulence index or not
+        user_defined_feature:boolean
+            user user defined features or not
+
+    Methods
+    -------
+    preprocess_data()
+        main method to do the feature engineering
+
+    """
+
+    def __init__(
+        self,
+        use_technical_indicator=True,
+        tech_indicator_list=config.TECHNICAL_INDICATORS_LIST,
+        use_turbulence=False
+    ):
+        self.use_technical_indicator = use_technical_indicator
+        self.tech_indicator_list = tech_indicator_list
+        self.use_turbulence = use_turbulence
+
+    def preprocess_data(self, df):
+        """main method to do the feature engineering
+        @:param config: source dataframe
+        @:return: a DataMatrices object
+        """
+        # clean data
+        df = self.clean_data(df)
+
+        # add technical indicators using stockstats
+        if self.use_technical_indicator:
+            df = self.add_technical_indicator(df)
+            print("Successfully added technical indicators")
+
+        # add turbulence index for multiple stock
+        if self.use_turbulence:
+            df = self.add_turbulence(df)
+            print("Successfully added turbulence index")
+
+        # fill the missing values at the beginning and the end
+        df = df.fillna(method="ffill").fillna(method="bfill")
+        df = df.replace([np.inf, -np.inf], 0.)
+        return df
+
+    def clean_data(self, data):
+        """
+        clean the raw data
+        deal with missing values
+        reasons: stocks could be delisted, not incorporated at the time step
+        :param data: (df) pandas dataframe
+        :return: (df) pandas dataframe
+        """
+        df = data.copy()
+        df = df.sort_values(["date", "tic"], ignore_index=True)
+        df.index = df.date.factorize()[0]
+        merged_closes = df.pivot_table(index="date", columns="tic", values="close")
+        merged_closes = merged_closes.dropna(axis=1)
+        tics = merged_closes.columns
+        df = df[df.tic.isin(tics)]
+        return df
+
+    def add_technical_indicator(self, data):
+        """
+        calculate technical indicators
+        use stockstats package to add technical inidactors
+        :param data: (df) pandas dataframe
+        :return: (df) pandas dataframe
+        """
+        df = data.copy()
+        df = df.sort_values(by=["tic", "date"])
+        stock = Sdf.retype(df.copy())
+        unique_ticker = stock.tic.unique()
+
+        for indicator in self.tech_indicator_list:
+            indicator_df = pd.DataFrame()
+            for i in range(len(unique_ticker)):
+                try:
+                    temp_indicator = stock[stock.tic == unique_ticker[i]][indicator]
+                    temp_indicator = pd.DataFrame(temp_indicator)
+                    temp_indicator["tic"] = unique_ticker[i]
+                    temp_indicator["date"] = df[df.tic == unique_ticker[i]][
+                        "date"
+                    ].to_list()
+                    indicator_df = indicator_df.append(
+                        temp_indicator, ignore_index=True
+                    )
+                except Exception as e:
+                    print(e)
+            df = df.merge(
+                indicator_df[["tic", "date", indicator]], on=["tic", "date"], how="left"
+            )
+        df = df.sort_values(by=["date", "tic"])
+        return df
+
+    def add_turbulence(self, data):
+        """
+        add turbulence index from a precalcualted dataframe
+        :param data: (df) pandas dataframe
+        :return: (df) pandas dataframe
+        """
+        df = data.copy()
+        turbulence_index = self.calculate_turbulence(df)
+        df = df.merge(turbulence_index, on="date")
+        df = df.sort_values(["date", "tic"]).reset_index(drop=True)
+        return df
+
+    # Equation (3)
+    def calculate_turbulence(self, data):
+        """calculate turbulence index based on dow 30"""
+        # can add other market assets
+        df = data.copy()
+        df_price_pivot = df.pivot(index="date", columns="tic", values="close")
+        # use returns to calculate turbulence
+        df_price_pivot = df_price_pivot.pct_change()
+
+        unique_date = df.date.unique()
+        # start after a year
+        start = 252
+        turbulence_index = [0] * start
+        # turbulence_index = [0]
+        count = 0
+        for i in range(start, len(unique_date)):
+            current_price = df_price_pivot[df_price_pivot.index == unique_date[i]]
+            # use one year rolling window to calcualte covariance
+            hist_price = df_price_pivot[
+                (df_price_pivot.index < unique_date[i])
+                & (df_price_pivot.index >= unique_date[i - 252])
+            ]
+            # Drop tickers which has number missing values more than the "oldest" ticker
+            filtered_hist_price = hist_price.iloc[
+                hist_price.isna().sum().min() :
+            ].dropna(axis=1)
+
+            cov_temp = filtered_hist_price.cov()
+            current_temp = current_price[[x for x in filtered_hist_price]] - np.mean(
+                filtered_hist_price, axis=0
+            )
+
+            # cov_temp = hist_price.cov()
+            # current_temp=(current_price - np.mean(hist_price,axis=0))
+
+            temp = current_temp.values.dot(np.linalg.pinv(cov_temp)).dot(
+                current_temp.values.T
+            )
+            if temp > 0:
+                count += 1
+                if count > 2:
+                    turbulence_temp = temp[0][0]
+                else:
+                    # avoid large outlier because of the calculation just begins
+                    turbulence_temp = 0
+            else:
+                turbulence_temp = 0
+            turbulence_index.append(turbulence_temp)
+
+        turbulence_index = pd.DataFrame(
+            {"date": df_price_pivot.index, "turbulence": turbulence_index}
+        )
+        return turbulence_index
